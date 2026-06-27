64b62904184b201f2f07f092ae78bb0f79e55271
JaneeWatermelon <sinitsyn.zena@yandex.ru>
2026-06-27 21:55:26 +0400

    Fixed header + added regular font

--- src/app/components/include/header.ts
+++ src/app/components/include/header.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
+  styleUrl: './header.css'
 })
 export class HeaderComponent {
   auth = inject(AuthService);
